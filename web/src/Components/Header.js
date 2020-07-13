3d66d603a440e9cb15ca79eaa486badff01cef15
Kenneth <kendevops@gmail.com>
2020-07-13 23:20:29 +0100

    Created header component

--- web/src/Components/Header.js
+++ web/src/Components/Header.js
@@ -1,24 +1,21 @@
 import React from "react";
 import image from "../img/mlh.png";
+import Search from "./Search";
 
 const Header = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        backgroundColor: "grey",
-        maxWidth: "100%",
-        color: "white",
-        justifyContent: "space-between",
-        padding: "0 20px",
-      }}
-    >
-      <img
-        src={image}
-        alt="MLH logo"
-        style={{ width: "150px", height: "150px" }}
-      />
-      <h1 style={{ paddingTop: "40px" }}>FellowBook</h1>
+    <div>
+      <img src={image} alt="mlh"/>
+      <Search />
+      <div>
+        <nav>
+          <ul>
+            <li>Fellowhunt</li>
+            <li>FellowJob</li>
+            <li>Fellowactivity</li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
