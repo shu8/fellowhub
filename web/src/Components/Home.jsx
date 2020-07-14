dfa54ce333e8b6d889800a1c6055d167398d1a6b
Shubham Jain <shubham@sjain.dev>
2020-07-14 22:19:30 +0100

    Lint & Use Octicons

--- web/src/Components/Home.jsx
+++ web/src/Components/Home.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
-import AppsIcon from "@material-ui/icons/Apps";
-import SettingsEthernetIcon from "@material-ui/icons/SettingsEthernet";
-import PeopleIcon from "@material-ui/icons/People";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import Profile from "./Profile";
 import SimpleAvatar from "./SimpleAvatar";
 
 import { TabNav } from "@primer/components";
+import { NoteIcon, PersonIcon, RepoIcon } from "@primer/octicons-react";
+
 function TabPanel(props) {
   const { children, tab, value, ...other } = props;
 
@@ -46,36 +45,50 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ScrollableTabsButtonForce() {
   const classes = useStyles();
-  const [tab, setTab] = React.useState('home');
+  const [tab, setTab] = React.useState("home");
 
   const createAvatars = () => {
     const avatars = [];
     for (let i = 0; i < 45; i++) {
-      avatars.push(<SimpleAvatar
-        bgPhoto={`https://picsum.photos/740/420/?random?${i}`}
-        cta="View profile"
-      />);
+      avatars.push(
+        <SimpleAvatar
+          bgPhoto={`https://picsum.photos/740/420/?random?${i}`}
+          cta="View profile"
+        />
+      );
     }
     return avatars;
-  }
+  };
 
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
       <Profile />
       <div className={classes.root}>
         <TabNav aria-label="Main">
-          <TabNav.Link href="#home" selected={tab === 'home'} onClick={() => setTab('home')}>
-            <AppsIcon /> Overview
+          <TabNav.Link
+            href="#home"
+            selected={tab === "home"}
+            onClick={() => setTab("home")}
+          >
+            <NoteIcon /> Overview
           </TabNav.Link>
-          <TabNav.Link href="#pods" selected={tab === 'pods'} onClick={() => setTab('pods')}>
-            <PeopleIcon /> Pods
+          <TabNav.Link
+            href="#pods"
+            selected={tab === "pods"}
+            onClick={() => setTab("pods")}
+          >
+            <PersonIcon /> Pods
           </TabNav.Link>
-          <TabNav.Link href="#projects" selected={tab === 'projects'} onClick={() => setTab('projects')}>
-            <SettingsEthernetIcon /> Projects
+          <TabNav.Link
+            href="#projects"
+            selected={tab === "projects"}
+            onClick={() => setTab("projects")}
+          >
+            <RepoIcon /> Projects
           </TabNav.Link>
         </TabNav>
 
-        <TabPanel tab={tab} value={'home'}>
+        <TabPanel tab={tab} value={"home"}>
           <div
             style={{
               display: "grid",
@@ -86,10 +99,10 @@ export default function ScrollableTabsButtonForce() {
             {createAvatars()}
           </div>
         </TabPanel>
-        <TabPanel tab={tab} value={'pods'}>
+        <TabPanel tab={tab} value={"pods"}>
           Pods
         </TabPanel>
-        <TabPanel tab={tab} value={'projects'}>
+        <TabPanel tab={tab} value={"projects"}>
           Projects
         </TabPanel>
       </div>
