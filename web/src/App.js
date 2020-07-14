68c8c158df4702a030be3a7c8ed1d0802bc2408e
Kenneth <kendevops@gmail.com>
2020-07-14 20:09:32 +0100

    Created more fellows

--- web/src/App.js
+++ web/src/App.js
@@ -1,40 +1,37 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
-import Card05 from "./Components/Avatar";
-import Avatars from "./Components/Avatar";
 import Home from "./Components/Home";
 import "font-awesome/css/font-awesome.min.css";
-import { fetchData } from "./Components";
-import octocat from "./img/octocat-white.png";
+// import { fetchData } from "./Components";
 
-const lower = (data) => (data ? data.toLowerCase() : "");
+// const lower = (data) => (data ? data.toLowerCase() : "");
 class App extends React.Component {
-  state = {
-    data: [],
-    search: "",
-    accessToken: null,
-  };
+  // state = {
+  //   data: [],
+  //   search: "",
+  //   accessToken: null,
+  // };
 
-  handleInput = (e) => {
-    this.setState({ search: e.target.value.toLowerCase() });
-  };
+  // handleInput = (e) => {
+  //   this.setState({ search: e.target.value.toLowerCase() });
+  // };
 
-  async componentDidMount() {
-    // Get saved access token from session storage
-    let accessToken = window.sessionStorage.getItem("accessToken");
+  // async componentDidMount() {
+  //   // Get saved access token from session storage
+  //   let accessToken = window.sessionStorage.getItem("accessToken");
 
-    if (window.location.search.includes("access_token")) {
-      accessToken = window.location.search.substr(1).split("=")[1];
-      window.sessionStorage.setItem("accessToken", accessToken);
-      window.location = "/";
-    }
+  //   if (window.location.search.includes("access_token")) {
+  //     accessToken = window.location.search.substr(1).split("=")[1];
+  //     window.sessionStorage.setItem("accessToken", accessToken);
+  //     window.location = "/";
+  //   }
 
-    const fetchedData = await fetchData(accessToken);
-    console.log(fetchedData);
-    if (!fetchedData || !fetchedData.length) accessToken = null;
-    this.setState({ data: fetchedData, accessToken });
-  }
+  //   const fetchedData = await fetchData(accessToken);
+  //   console.log(fetchedData);
+  //   if (!fetchedData || !fetchedData.length) accessToken = null;
+  //   this.setState({ data: fetchedData, accessToken });
+  // }
 
   render() {
     return (
