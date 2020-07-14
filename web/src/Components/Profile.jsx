c5b9e55017e32cf58353d2603cb6105a5769d0ca
Kenneth <kendevops@gmail.com>
2020-07-14 17:00:14 +0100

    designed the Profile page and the fellows

--- web/src/Components/Profile.jsx
+++ web/src/Components/Profile.jsx
@@ -10,17 +10,58 @@ function profile(props) {
           src={image}
           alt="MLH"
           style={{
-            width: "350px",
+            width: "380px",
+            height: "350px",
             borderStyle: "solid",
             borderColor: "#000",
             borderRadius: "100%",
             margin: "20px",
           }}
         />
-        <div>
-          <h3>Fellows</h3>
-          <h3>Mentors</h3>
-          <h3>MLH Staffs</h3>
+        <div
+          style={{
+            margin: "20px",
+            textAlign: "left",
+            borderStyle: "solid",
+            backgroundColor: "grey",
+          }}
+        >
+          <h3>
+            Fellows{" "}
+            <span
+              style={{
+                backgroundColor: "grey",
+                color: "white",
+                padding: "10px",
+              }}
+            >
+              160
+            </span>
+          </h3>
+          <h3>
+            Mentors{" "}
+            <span
+              style={{
+                backgroundColor: "grey",
+                color: "white",
+                padding: "10px",
+              }}
+            >
+              20
+            </span>
+          </h3>
+          <h3>
+            MLH Staffs{" "}
+            <span
+              style={{
+                backgroundColor: "grey",
+                color: "white",
+                padding: "10px",
+              }}
+            >
+              5
+            </span>
+          </h3>
         </div>
       </div>
     </div>
