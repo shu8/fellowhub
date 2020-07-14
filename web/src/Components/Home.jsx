26e99013c5ec04b8d700d92a97ca31019ff0c0e5
Kenneth <kendevops@gmail.com>
2020-07-14 13:24:05 +0100

    Created the header and the home elements

--- web/src/Components/Home.jsx
+++ web/src/Components/Home.jsx
@@ -1,20 +1,45 @@
 import React from "react";
-import { SubNav } from "@primer/components";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import PhoneIcon from "@material-ui/icons/Phone";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
+import AccountTreeIcon from "@material-ui/icons/AccountTree";
 
-import Styles from "./Home.module.css";
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+    maxWidth: 650,
+  },
+});
 
-function Home(props) {
+export default function IconLabelTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <SubNav aria-label="Main">
-      <SubNav.Links>
-        <SubNav.Link href="#home" selected>
-          Home
-        </SubNav.Link>
-        <SubNav.Link href="#documentation">Documentation</SubNav.Link>
-        <SubNav.Link href="#support">Support</SubNav.Link>
-      </SubNav.Links>
-    </SubNav>
+    <div>
+      <Paper square className={classes.root}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="MEDIUM"
+          indicatorColor="secondary"
+          textColor="secondary"
+          aria-label="icon label tabs example"
+        >
+          <Tab icon={<PhoneIcon />} label="OVERVIEW" />
+          <Tab icon={<FavoriteIcon />} label="PODS" />
+          <Tab icon={<PersonPinIcon />} label="PROJECTS" />
+          <Tab icon={<AccountTreeIcon />} label="FELLOWS" />
+        </Tabs>
+      </Paper>
+    </div>
   );
 }
-
-export default Home;
