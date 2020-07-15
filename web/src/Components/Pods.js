f6b545387ee8ba9bf5dcd3395c9b6fd6443b5e0f
Kenneth <kendevops@gmail.com>
2020-07-15 01:41:45 +0100

    created pods basic

---
+++ web/src/Components/Pods.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Pods = (props) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        backgroundColor: "#C0C0C0	",
+      }}
+    >
+      <div>
+        <p class="h2">{props.name}</p>
+        <p class="h5">{props.description}</p>
+      </div>
+      <div
+        class="AvatarStack AvatarStack--three-plus"
+        style={{ paddingTop: "30px", paddingRight: "190px" }}
+      >
+        <div
+          class="AvatarStack-body tooltipped tooltipped-se tooltipped-align-left-1"
+          aria-label="octocat, octocat, and octocat"
+        >
+          <img
+            class="avatar"
+            height="40"
+            alt="@octocat"
+            src="https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png"
+            width="20"
+          />
+          <img
+            class="avatar"
+            height="20"
+            alt="@octocat"
+            src="https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png"
+            width="20"
+          />
+          <img
+            class="avatar"
+            height="20"
+            alt="@octocat"
+            src="https://user-images.githubusercontent.com/334891/29999089-2837c968-9009-11e7-92c1-6a7540a594d5.png"
+            width="20"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pods;
