39881d12b871b7b1f84da8b229e3d4ebf640b6e8
Shubham Jain <shubham@sjain.dev>
2020-07-17 11:54:20 +0100

    Optimise API requests
    - We don't need to make as many requests as we were making
    - We can send the requests in parallel to save time

--- web/src/Components/index.js
+++ web/src/Components/index.js
@@ -2,22 +2,6 @@ import axios from "axios";
 
 const apiUrl = "https://ld48eii9kk.execute-api.eu-central-1.amazonaws.com/dev";
 
-export const getPodmates = async (accessToken, podId) => {
-	const endpoint = apiUrl + "/random?query=" + podId;
-
-  try {
-    const response = await fetch(endpoint, {
-      headers: {
-        Authorization: accessToken
-      },
-    });
-    return response.json();
-  } catch (error) {
-    console.log(error);
-    return [];
-  }
-};
-
 export const fetchActiveFellow = async (accessToken) => {
   try {
     const response = await axios.get(`https://api.github.com/user`, {
@@ -26,20 +10,6 @@ export const fetchActiveFellow = async (accessToken) => {
       },
     });
     return response.data.login; // githubId for active fellow
-  } catch (error) {
-    console.log(error);
-    return "";
-  }
-};
-
-export const fetchSingleFellow = async (accessToken, githubId) => {
-  try {
-    const response = await axios.get(`${apiUrl}/fellows/${githubId}`, {
-      headers: {
-        Authorization: accessToken,
-      },
-    });
-    return response.data;
   } catch (error) {
     console.log(error);
     return "";
